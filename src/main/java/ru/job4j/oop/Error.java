89457ffcc5d4c803fc84fb6cf4525882bf8227c2
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-08-18 00:24:43 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String messag;
+
+    public Error() {}
+
+    public Error(boolean active, int status, String messag) {
+        this.active = active;
+        this.status = status;
+        this.messag = messag;
+    }
+
+    public void printInfo() {
+        System.out.println("Актив: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + messag);
+    }
+
+    public static void main(String[] args) {
+        new Error().printInfo();
+        new Error(false, -1, "false-1").printInfo();
+        new Error(true, 1, "true1").printInfo();
+    }
+}
