7053ee1b0d030c8ad231490e58c86fe78ba90a31
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-08-17 23:31:03 +0300

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4/oop/Cat.java
@@ -0,0 +1,29 @@
+package ru.job4.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.show();
+    }
+}
