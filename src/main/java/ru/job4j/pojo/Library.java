391245ff126918e0c025cfe0d970fbdcce503956
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-08-20 10:38:36 +0300

    3. Массивы и модели. [#174892] fix

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -6,7 +6,7 @@ public class Library {
         Book book2 = new Book("Теремок", 20);
         Book book3 = new Book("Курочка Ряба", 30);
         Book book4 = new Book("Clean code", 40);
-        Book[] books = { book1, book2, book3, book4 };
+        Book[] books = new Book[]{book1, book2, book3, book4};
         for (int i = 0; i < books.length; i++) {
             System.out.println(books[i].getName() + ", " + books[i].getCount());
         }
