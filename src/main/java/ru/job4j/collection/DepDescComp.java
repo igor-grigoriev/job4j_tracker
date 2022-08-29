0df26e2f51f06dc3468c830df832320a3feca150
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-08-29 08:02:05 +0300

    2. Отсортировать департаменты [#34352] fix

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,14 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] left = o1.split("/");
         String[] right = o2.split("/");
         int res = right[0].compareTo(left[0]);
-        if (res == 0) {
-            for (int i = 1; i < Math.min(left.length, right.length); i++) {
-                res = left[i].compareTo(right[i]);
-                if (res != 0) {
-                    break;
-                }
-            }
-        }
-        return (res != 0) ? res : Integer.compare(left.length, right.length);
+        return (res != 0) ? res : o1.compareTo(o2);
     }
 }
